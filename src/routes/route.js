f88f2ee1d60c319741fe258186b4268eecbf3a00
akashGg99 <aakkash15r@gmail.com>
2022-10-18 14:11:38 +0530

    Code in routejs of params and getapi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
@@ -37,7 +38,133 @@ router.get('/student-details/:name', function(req, res){
     console.log("This is the request ", requestParams)
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
+    res.send('Dummy response nahi. Baat ye hai ki url me aapko dena hai input naam ka jo run karte hi console hojayega (agar aapne console likha ho) as object key:value (value jo input diya hai url me : ke baad,key aapka var name hai) pair as by default name req.params. yah ek obj hai jo ban jata hai aur print ho jata hai.')
 })
+
+
+//Humari pehle Api banana shuru kiya jaaye yaha se....  OUR FIRST EVER API BUILD.....
+
+
+//Problem 1
+router.get('/movies', function(req,res){
+    let ArrMovies = ["Border", "Suryavansham", "Bahubali", "Gadar"];
+    console.log=(ArrMovies);       //console on terminal after res is printed .
+    res.send(ArrMovies)             // Response Appears on the clients display
+})
+
+//Problem 2
+router.get('/movies/:indexNumber', function(req,res){
+    let ArrMovies = ["Border", "Suryavansham", "Bahubali", "Gadar"];
+    const index = req.params.indexNumber      //value gets stored by default in OBJ req.params thus . to get the value
+    res.send(ArrMovies[index])                // Response Appears on the clients display
+})
+
+
+
+//Problem 3
+router.get('/moviesfrom/:indexNumber', function(req,res) {
+    let ArrMovies = ["Border", "Suryavansham", "Bahubali", "Gadar"];
+    const index = req.params.indexNumber
+
+    if (index>= ArrMovies.length){
+        res.send("use a valid index")
+    }else{
+        res.send(ArrMovies[index])
+    }
+})
+
+
+//Problem 4
+router.get('/films', function(req,res){
+    let ArrMovies = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+
+    
+    res.send(ArrMovies)             // Response Appears on the clients display
+})
+
+
+
+//Problem 5......
+router.get('/films/:filmid', function (req, res){
+    let ArrMovies = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }];
+       
+
+        for(let i=0;i<ArrMovies.length;i++){
+             if(ArrMovies[i].id == req.params.filmid){
+                
+                res.send(ArrMovies[i])
+              }
+              else{"Movie does not exixt."}
+            } 
+           
+
+
+            
+
+    //    //Using find
+    //    const filamid = req.params.filmid
+    //   const filmovies = ArrMovies.find(filamid =>filamid == ArrMovies[filamid].id)
+    //   if(filmovies){
+    //     res.send(filmovies)
+    //   }else{
+    //     res.send("No such movie exists.")
+    //   }
+
+
+
+        // ArrMovies.map((elem,index) => {
+        //     if(index==req.params.filmid){
+        //         res.send(elem)
+        //     }
+        //     else{
+        //         res.send("Movie does not exist")
+        //     }
+        // });
+
+
+
+
+        // // let count = 0
+        // for(let i=0;i<ArrMovies.length;i++){
+        //      if(ArrMovies[i].id == req.params.filmid){
+        //         // count++
+        //         res.send(ArrMovies[i])
+        //       }
+        //       else{"Movie does not exixt."}
+        //     } 
+        //     //  }
+        //     //      if(count==0)
+        //     //  res.send("id is not match")
+
+});
+
+
+
+
+
 
 module.exports = router;
