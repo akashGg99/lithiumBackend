917b26382ac4ebe85f76cb132c0fddf3a8201773
akashGg99 <aakkash15r@gmail.com>
2022-11-10 02:17:28 +0530

    just saving for future ref

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -24,3 +24,54 @@ module.exports.mid1= mid1
 module.exports.mid2= mid2
 module.exports.mid3= mid3
 module.exports.mid4= mid4
+
+
+
+
+
+
+//  .................. Some more http codes & try catch examples    ...... (comment code below if not in use.)
+
+const authenticate = function(req, res, next) {
+
+    try {
+    //check the token in request header
+    //validate this token
+    const token = req.headers["x-auth-token"]
+    console.log(`800808+++`,token)
+    if (!token){ return res.status(400).send({ status: false, msg: "Token must be present" })}
+    else{
+    next()}
+
+    }
+
+    catch(anything){
+        res.status(500).send({msg:`Server side error in Middleware described below.`, error : anything.message})
+    }
+    
+}
+
+const authorise = function(req, res, next) {
+    try{
+
+    // comapre the logged in user's id and the id in request
+    const decodedToken = jwt.verify(token, "SecretCode")
+    if (!decodedToken) return res.status(400).send({ status: false, msg: "Token is invalid" })
+
+    const userId = req.params.userId
+    //Authorization
+    const userLoggedIn = decodedToken.user
+    if (userLoggedIn != userId){ return res.status(401).send({ status: false, msg: "User loggedIn is not allowed to update details" })}
+    else{
+    next()}
+
+}
+    catch(anything){
+        res.status(500).send({msg:`Server side error in Middleware2 described below.`, error : anything.message})
+    }
+    
+}
+
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
